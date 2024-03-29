eb41fdd2ff59eda88ed7cff75034231d143105fc
Marco Meijer <marcomeijerm@gmail.com>
2024-03-29 11:06:56 +0100

    feat: added body parser and etag

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const compression = require('compression');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
 app.use(compression());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('../'));
+app.set('etag', 'strong');
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
